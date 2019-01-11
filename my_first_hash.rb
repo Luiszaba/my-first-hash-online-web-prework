b128177759c4f8505edee41c3f344f1404d8f90f
Luis Zabala <luis.zabala076@gmail.com>
2019-01-11 03:02:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-myHash  = "Age" => 29
+myHash  = {"Age" => 29}
   
 end
 
@@ -9,9 +9,9 @@ def shipping_manifest(manifest)
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-manifest = {
-"whale bone corsets" => 5
-"porcelain vases" => 2
+the_manifest = {
+"whale bone corsets" => 5,
+"porcelain vases" => 2,
 "oil paintings" => 3}
 end
 
@@ -33,7 +33,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest = ""
 
 
   # add 4 gun powder to the shipping_manifest hash below
